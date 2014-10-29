ddf0bb4d1eca04bdf1b66f35fcada064b889ff6d
michael <bloodfaiz@yahoo.com>
2014-10-28 20:08:43 -0400

    Small clean ups

--- Trinity Of Warcraft/Trinity Of Warcraft.cpp
+++ Trinity Of Warcraft/Trinity Of Warcraft.cpp
@@ -9,12 +9,10 @@
 
 #define MAX_LOADSTRING 100
 
-// Global Variables:
 HINSTANCE hInst;								// current instance
 TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
 TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
 
-// Forward declarations of functions included in this code module:
 ATOM				MyRegisterClass(HINSTANCE hInstance);
 BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
@@ -28,16 +26,15 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
- 	// TODO: Place code here.
 	MSG msg;
 	HACCEL hAccelTable;
 
-	// Initialize global strings
+
 	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
 	LoadString(hInstance, IDC_TRINITYOFWARCRAFT, szWindowClass, MAX_LOADSTRING);
 	MyRegisterClass(hInstance);
 
-	// Perform application initialization:
+
 	if (!InitInstance (hInstance, nCmdShow))
 	{
 		return FALSE;
@@ -45,7 +42,7 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 
 	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_TRINITYOFWARCRAFT));
 
-	// Main message loop:
+
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
 		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
@@ -79,22 +76,12 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 	return RegisterClassEx(&wcex);
 }
 
-//
-//   FUNCTION: InitInstance(HINSTANCE, int)
-//
-//   PURPOSE: Saves instance handle and creates main window
-//
-//   COMMENTS:
-//
-//        In this function, we save the instance handle in a global variable and
-//        create and display the main program window.
-//
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
 	HWND hWnd;
 	
 
-   hInst = hInstance; // Store instance handle in our global variable
+   hInst = hInstance; 
 
    hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
       CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
@@ -105,46 +92,19 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    }
    
    
-   ShowWindow(hWnd, nCmdShow);
-   RECT rect;
-	GetWindowRect(hWnd, &rect);
-	int width = rect.right - rect.left;
-	int height = rect.bottom - rect.top;
-
-	/*if (WM_LBUTTONDOWN == true)
-	{
-		CreateWindowEx(NULL, L"Edit", L"", WS_VISIBLE | WS_CHILD, width / 2.2f, height / 2.2f, 200, 20, hWnd, NULL, hInstance, NULL);
-		UpdateWindow(hWnd);
-	}
-	else
-	{
-		CreateWindowEx(NULL, L"Edit", L"Username", WS_VISIBLE | WS_CHILD, width / 2.2f, height / 2.2f, 200, 20, hWnd, NULL, hInstance, NULL);
-		UpdateWindow(hWnd);
-	}*/
-
-   UpdateWindow(hWnd);
+    ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
 
    return TRUE;
 }
 
-//
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  PURPOSE:  Processes messages for the main window.
-//
-//  WM_COMMAND	- process the application menu
-//  WM_PAINT	- Paint the main window
-//  WM_DESTROY	- post a quit message and return
-//
-//
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	PAINTSTRUCT ps;		
 	static HBITMAP bmpSource = NULL;
     static HDC hdcSource = NULL;     
     HDC hdcDestination;
-	RECT rect;
-	std::cout << "fhgfghfghfga";
+	RECT rect;	
 
 	GetWindowRect(hWnd, &rect);
 	int width = rect.right - rect.left;
